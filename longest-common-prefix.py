788776a1485d9143b46a8b7fa88d83f7ba59849e
Shri <skt1598@gmail.com>
2022-10-30 20:30:11 +0530

    longestCommonPrefix

---
+++ longest-common-prefix.py
@@ -0,0 +1,16 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string ""
+
+def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        shortest = min(strs,key=len)
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                if other[i] != ch:
+                    return shortest[:i]
+        return shortest
